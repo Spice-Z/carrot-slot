bd8685ec171f4d79ac748be6705adb389e201760
小倉優悟 <jigenbakdaaan@gmail.com>
2019-01-20 16:49:49 +0900

    add:vue-analyticsno

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueAnalytics from 'vue-analytics'
+
+Vue.use(VueAnalytics, {
+  id: 'UA-132772360-1'
+})
 
 Vue.config.productionTip = false
 
